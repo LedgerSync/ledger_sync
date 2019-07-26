b4e714fa98274320dd70e1855fc9ef8d7483e3a6
Ryan Jackson <ryanwjackson@gmail.com>
2019-07-26 22:47:45 +0200

    Use bundle bump gem

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
-  bump $1 && git push && bundle exec rake build && bundle exec rake release
+  bundle exec bump $1 && git push && bundle exec rake build && bundle exec rake release
 else
   echo 'PLEASE COMMIT ALL CHANGES BEFORE RELEASING.'
   exit 1
