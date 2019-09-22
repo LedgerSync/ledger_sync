d135a4e95ca15ca845c42460bc57ea53fb633f6f
Ryan Jackson <ryanwjackson@gmail.com>
2019-09-22 10:40:04 -0400

    Fix tests

--- spec/adaptors/quickbooks_online/searcher_spec.rb
+++ spec/adaptors/quickbooks_online/searcher_spec.rb
@@ -11,11 +11,12 @@ RSpec.describe LedgerSync::Adaptors::QuickBooksOnline::Searcher do
   subject { described_class.new(adaptor: adaptor, query: query) }
 
   describe '#next_searcher' do
-    it { expect { subject.next_searcher }.to raise_error(NotImplementedError) }
+    it { expect(subject.next_searcher.pagination).to eq({ limit: 10, offset: 11 }) }
   end
 
   describe '#previous_searcher' do
-    it { expect { subject.previous_searcher }.to raise_error(NotImplementedError) }
+    it { expect(subject.previous_searcher).to be_nil }
+    it { expect(subject.next_searcher.previous_searcher.pagination).to eq({ limit: 10, offset: 1 }) }
   end
 
   describe '#resources' do
