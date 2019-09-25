27b8052de35562dee25920a531bbb3ddc8520d51
Ryan <ryanwjackson@users.noreply.github.com>
2019-09-25 11:43:45 -0400

    DRY up QBO searchers (#30)
    * DRY up QBO searchers
    
    * Fix tests

---
+++ spec/adaptors/quickbooks_online/searcher_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+support :adaptor_helpers
+
+RSpec.describe LedgerSync::Adaptors::QuickBooksOnline::Searcher do
+  include AdaptorHelpers
+
+  let(:adaptor) { test_adaptor }
+  let(:query) { 'qqq' }
+
+  subject { described_class.new(adaptor: adaptor, query: query) }
+
+  describe '#next_searcher' do
+    it { expect(subject.next_searcher.pagination).to eq({ limit: 10, offset: 11 }) }
+  end
+
+  describe '#previous_searcher' do
+    it { expect(subject.previous_searcher).to be_nil }
+    it { expect(subject.next_searcher.previous_searcher.pagination).to eq({ limit: 10, offset: 1 }) }
+  end
+
+  describe '#resources' do
+    it { expect { subject.resources }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#search' do
+    it { expect { subject.search }.to raise_error(NotImplementedError) }
+  end
+end
