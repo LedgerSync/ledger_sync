e0fe6c968ecdd8c1a61855dc2672f30222e0ba25
Jozef Vaclavik <jozefvaclavik@users.noreply.github.com>
2019-10-11 15:41:30 +0200

    Add Deposit resource and Operations (#62)

---
+++ spec/integrations/quickbooks_online/deposits/create_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+support :input_helpers
+support :adaptor_helpers
+support :quickbooks_helpers
+
+RSpec.describe 'quickbooks_online/deposits/create', type: :feature do
+  include InputHelpers
+  include AdaptorHelpers
+  include QuickbooksHelpers
+
+  before {
+    stub_create_deposit
+  }
+
+  let(:account) do
+    LedgerSync::Account.new(account_resource({ledger_id: '123'}))
+  end
+
+  let(:line_item_1) do
+    LedgerSync::DepositLineItem.new(deposit_line_item_resource({account: account}))
+  end
+
+  let(:line_item_2) do
+    LedgerSync::DepositLineItem.new(deposit_line_item_resource({account: account}))
+  end
+
+  let(:resource) do
+    LedgerSync::Deposit.new(
+      deposit_resource(
+        {
+          to_account: account,
+          line_items: [
+            line_item_1,
+            line_item_2
+          ]
+        }
+      )
+    )
+  end
+
+  let(:input) do
+    {
+      adaptor: quickbooks_adaptor,
+      resource: resource
+    }
+  end
+
+  context '#perform' do
+    subject { LedgerSync::Adaptors::QuickBooksOnline::Deposit::Operations::Create.new(**input).perform }
+    it { expect(subject).to be_success }
+    it { expect(subject).to be_a(LedgerSync::OperationResult::Success)}
+  end
+end
---
+++ spec/integrations/quickbooks_online/deposits/find_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+support :input_helpers
+support :adaptor_helpers
+support :quickbooks_helpers
+
+RSpec.describe 'quickbooks_online/deposits/find', type: :feature do
+  include InputHelpers
+  include AdaptorHelpers
+  include QuickbooksHelpers
+
+  before {
+    stub_find_deposit
+  }
+
+  let(:account) do
+    LedgerSync::Account.new(account_resource({ledger_id: '123'}))
+  end
+
+  let(:line_item_1) do
+    LedgerSync::DepositLineItem.new(deposit_line_item_resource({account: account}))
+  end
+
+  let(:line_item_2) do
+    LedgerSync::DepositLineItem.new(deposit_line_item_resource({account: account}))
+  end
+
+  let(:resource) do
+    LedgerSync::Deposit.new(
+      deposit_resource(
+        {
+          ledger_id: '123',
+          to_account: account,
+          line_items: [
+            line_item_1,
+            line_item_2
+          ]
+        }
+      )
+    )
+  end
+
+  let(:input) do
+    {
+      adaptor: quickbooks_adaptor,
+      resource: resource
+    }
+  end
+
+  context '#perform' do
+    subject { LedgerSync::Adaptors::QuickBooksOnline::Deposit::Operations::Find.new(**input).perform }
+    it { expect(subject).to be_success }
+    it { expect(subject).to be_a(LedgerSync::OperationResult::Success)}
+  end
+end
