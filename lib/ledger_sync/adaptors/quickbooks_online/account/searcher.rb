d38ecad20e73d68d8bf9dacb5bdfcad9a235f3d4
Matt Marcus <matt.g.marcus@gmail.com>
2019-10-13 18:55:52 -0700

    add missing account type and make nil safe

--- lib/ledger_sync/adaptors/quickbooks_online/account/searcher.rb
+++ lib/ledger_sync/adaptors/quickbooks_online/account/searcher.rb
@@ -28,9 +28,9 @@ module LedgerSync
                   LedgerSync::Account.new(
                     ledger_id: c.fetch('Id'),
                     name: c.fetch('Name'),
-                    classification: Mapping::ACCOUNT_CLASSIFICATIONS.invert[c.fetch('Classification')],
-                    account_type: Mapping::ACCOUNT_TYPES.invert[c.fetch('AccountType')],
-                    account_sub_type: Mapping::ACCOUNT_SUB_TYPES.invert[c.fetch('AccountSubType')]
+                    classification: Mapping::ACCOUNT_CLASSIFICATIONS.invert[c.fetch('Classification')] || c.fetch('Classification'),
+                    account_type: Mapping::ACCOUNT_TYPES.invert[c.fetch('AccountType')] || c.fetch('AccountType'),
+                    account_sub_type: Mapping::ACCOUNT_SUB_TYPES.invert[c.fetch('AccountSubType')] || c.fetch('AccountSubType')
                   )
                 end
             end
