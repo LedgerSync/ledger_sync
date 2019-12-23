4aaaa937d53ebb0781fbd0c715686adc4f8a560d
Ryan Jackson <ryanwjackson@gmail.com>
2019-12-23 17:03:07 +0100

    Fix factories

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :account, class: LedgerSync::Account do
-    sequence(:name) { |n| "Test Account #{n}" }
+  factory :bill, class: LedgerSync::Bill do
+    references_one :account
+
+    sequence(:memo) { |n| "Memo #{n}" }
   end
 end
