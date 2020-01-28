7c768aa5eed01cc53c3342a2bce68654a29f1a84
Ryan Jackson <ryanwjackson@gmail.com>
2020-01-28 13:54:20 +0100

    Add tests for resource, serializer, and validation contract

--- spec/adaptors/operation_spec.rb
+++ spec/adaptors/operation_spec.rb
@@ -16,6 +16,21 @@ RSpec.describe LedgerSync::Adaptors::Operation do
                 resource_attribute: :foo
     end
   end
+  let(:validation_contract) do
+    Class.new(LedgerSync::Adaptors::Contract) do
+      params do
+        required(:foo).filled(:string)
+      end
+    end
+  end
+  let(:custom_resource_class) do
+    Object.const_set(
+      "#{test_run_id}TestCustomResource",
+      Class.new(LedgerSync::Customer) do
+        attribute :foo, type: LedgerSync::Type::String
+      end
+    )
+  end
   let(:operation) do
     operation_class.new(
       adaptor: adaptor,
@@ -81,6 +96,44 @@ RSpec.describe LedgerSync::Adaptors::Operation do
     end
   end
 
+  describe '#ledger_serializer_class' do
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.ledger_serializer_class).to eq(LedgerSync::Adaptors::NetSuite::Customer::LedgerSerializer)
+    end
+
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        ledger_serializer_class: nil,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.ledger_serializer_class).to eq(LedgerSync::Adaptors::NetSuite::Customer::LedgerSerializer)
+    end
+
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        ledger_serializer_class: serializer_class,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.ledger_serializer_class).to eq(serializer_class)
+    end
+
+    it do
+      expect do
+        operation_class.new(
+          adaptor: netsuite_adaptor,
+          ledger_serializer_class: 'asdf',
+          resource: FactoryBot.create(:customer)
+        )
+      end.to raise_error(LedgerSync::Error::UnexpectedClassError)
+    end
+  end
+
   describe '#perform' do
     subject { operation.perform }
 
@@ -90,6 +143,42 @@ RSpec.describe LedgerSync::Adaptors::Operation do
       allow(operation).to receive(:operate) { raise LedgerSync::Error.new(message: 'Test') }
       expect(subject).to be_failure
       expect(subject.error.message).to eq('Test')
+    end
+  end
+
+  describe '#resource' do
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.resource).to be_a(LedgerSync::Customer)
+    end
+
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        resource: custom_resource_class.new
+      )
+      expect(op.resource).to be_a(custom_resource_class)
+    end
+
+    it do
+      expect do
+        operation_class.new(
+          adaptor: netsuite_adaptor,
+          resource: FactoryBot.create(:expense)
+        )
+      end.to raise_error(LedgerSync::Error::UnexpectedClassError)
+    end
+
+    it do
+      expect do
+        operation_class.new(
+          adaptor: netsuite_adaptor,
+          resource: nil
+        )
+      end.to raise_error(LedgerSync::Error::UnexpectedClassError)
     end
   end
 
@@ -104,6 +193,44 @@ RSpec.describe LedgerSync::Adaptors::Operation do
     it do
       subject.perform
       expect(subject).not_to be_valid
+    end
+  end
+
+  describe '#validation_contract' do
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.validation_contract).to eq(operation_class::Contract)
+    end
+
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        validation_contract: nil,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.validation_contract).to eq(operation_class::Contract)
+    end
+
+    it do
+      op = operation_class.new(
+        adaptor: netsuite_adaptor,
+        validation_contract: validation_contract,
+        resource: FactoryBot.create(:customer)
+      )
+      expect(op.validation_contract).to eq(validation_contract)
+    end
+
+    it do
+      expect do
+        operation_class.new(
+          adaptor: netsuite_adaptor,
+          validation_contract: 'asdf',
+          resource: FactoryBot.create(:customer)
+        )
+      end.to raise_error(LedgerSync::Error::UnexpectedClassError)
     end
   end
 end
