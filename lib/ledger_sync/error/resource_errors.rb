8a4ef39ce69dc4ed67b2a679339ea1f81f433d94
Ryan Jackson <ryanwjackson@gmail.com>
2020-05-07 08:54:09 +0200

    Simplify

--- lib/ledger_sync/error/resource_errors.rb
+++ lib/ledger_sync/error/resource_errors.rb
@@ -6,36 +6,44 @@ module LedgerSync
       attr_reader :attribute, :resource_class, :value
 
       def initialize(args = {})
-        @attribute = args.fetch(:attribute)
+        @attribute      = args.fetch(:attribute)
         @resource_class = args.fetch(:resource_class)
-        @value = args.fetch(:value)
+        @value          = args.fetch(:value)
 
-        type_resource_class = if attribute.is_a?(LedgerSync::ResourceAttribute::Reference)
-                                attribute.type.resource_class
-                              else
-                                attribute.type.valid_classes
-                              end
-
-        message = "Attribute #{attribute.name} for #{resource_class.name} should be "
-
-        message += case attribute
-                   when LedgerSync::ResourceAttribute::Reference::Many
-                     invalid_classes = value.reject { |e| e.is_a?(type_resource_class) }.map(&:class)
-                     if type_resource_class.is_a?(Array)
-                       "an array of one or more of the following: #{type_resource_class.name}.  Given array containing: #{invalid_classes.join(', ')}"
-                     else
-                       "an array of #{type_resource_class.name}.  Given array containing: #{invalid_classes.join(', ')}"
-                     end
-                   else
-                     if type_resource_class.is_a?(Array)
-                       "one of the following: #{type_resource_class.map(&:name).join(', ')}.  Given #{value.class.name}"
-                     else
-                       "a #{type_resource_class.name}.  Given #{value.class.name}"
-                     end
-                   end
-
-        super(message: message)
+        super(message: generate_message)
       end
+
+      private
+
+      def generate_message
+        ret = "Attribute #{attribute.name} for #{resource_class.name} should be "
+
+        ret += case attribute
+               when LedgerSync::ResourceAttribute::Reference::Many
+                 invalid_classes = value.reject { |e| e.is_a?(type_resource_class) }.map(&:class)
+                 if type_resource_class.is_a?(Array)
+                   "an array of one or more of the following: #{type_resource_class.name}.  Given array containing: #{invalid_classes.join(', ')}"
+                 else
+                   "an array of #{type_resource_class.name}.  Given array containing: #{invalid_classes.join(', ')}"
+                 end
+               else
+                 if type_resource_class.is_a?(Array)
+                   "one of the following: #{type_resource_class.map(&:name).join(', ')}.  Given #{value.class.name}"
+                 else
+                   "a #{type_resource_class.name}.  Given #{value.class.name}"
+                 end
+               end
+
+        ret
+      end
+
+      def type_resource_class
+        @type_resource_class ||= if attribute.is_a?(LedgerSync::ResourceAttribute::Reference)
+                                   attribute.type.resource_class
+                                 else
+                                   attribute.type.valid_classes
+                                 end
+    end
     end
   end
 
