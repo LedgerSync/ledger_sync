5c0e62cfe88e88ea916f57112a4212bcd627a0c1
Sean Bolton <seanbolton95@gmail.com>
2020-05-26 13:35:34 -0700

    Fix typo in QBO deserializer, add spec

--- lib/ledger_sync/ledgers/netsuite/resources/account.rb
+++ lib/ledger_sync/ledgers/netsuite/resources/account.rb
@@ -29,35 +29,6 @@ module LedgerSync
           'non_posting' => 'NonPosting'
         }.freeze
 
-        TYPES_TO_CLASSIFICATION_MAPPING = {
-          'bank' => 'asset',
-          'other_current_assets' => 'asset',
-          'fixed_asset' => 'asset',
-          'other_asset' => 'asset',
-          'accounts_receivable' => 'asset',
-          'equity' => 'equity',
-          'expense' => 'expense',
-          'other_expense' => 'expense',
-          'cost_of_goods_sold' => 'expense',
-          'accounts_payable' => 'liability',
-          'credit_card' => 'liability',
-          'long_term_liability' => 'liability',
-          'other_current_liability' => 'liability',
-          'income' => 'revenue',
-          'other_income' => 'revenue',
-          'deferred_expense' => 'asset',
-          'unbilled_recievable' => 'assets',
-          'deferred_revenue' => 'liability'
-        }.freeze
-
-        CLASSIFICATIONS = {
-          'asset' => 'Asset',
-          'equity' => 'Equity',
-          'expense' => 'Expense',
-          'liability' => 'Liability',
-          'revenue' => 'Revenue'
-        }.freeze
-
         attribute :name, type: Type::String
         attribute :classification, type: Type::String
         attribute :account_type, type: Type::StringFromSet.new(values: TYPES.keys)
