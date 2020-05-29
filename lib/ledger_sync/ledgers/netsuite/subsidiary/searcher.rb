9e927d45a1c39e5df4b4ac225fe4657c3260aee1
Sean Bolton <seanbolton95@gmail.com>
2020-05-28 17:01:20 -0700

    Add NS Subsidiary Searcher

---
+++ lib/ledger_sync/ledgers/netsuite/subsidiary/searcher.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module LedgerSync
+  module Ledgers
+    module NetSuite
+      class Subsidiary
+        class Searcher < Searcher
+        end
+      end
+    end
+  end
+end
