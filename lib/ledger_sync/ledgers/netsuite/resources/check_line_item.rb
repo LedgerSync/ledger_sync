9003783f767d8984625a8affab4aba33745a7f9e
Sean Bolton <seanbolton95@gmail.com>
2020-05-29 19:01:25 -0700

    Use specific module naming in line item references

--- lib/ledger_sync/ledgers/netsuite/resources/check_line_item.rb
+++ lib/ledger_sync/ledgers/netsuite/resources/check_line_item.rb
@@ -4,9 +4,9 @@ module LedgerSync
   module Ledgers
     module NetSuite
       class CheckLineItem < NetSuite::Resource
-        references_one :account, to: Account
-        references_one :ledger_class, to: LedgerClass
-        references_one :department, to: Department
+        references_one :account, to: NetSuite::Account
+        references_one :ledger_class, to: NetSuite::LedgerClass
+        references_one :department, to: NetSuite::Department
         attribute :amount, type: Type::Float
         attribute :memo, type: Type::String
       end
