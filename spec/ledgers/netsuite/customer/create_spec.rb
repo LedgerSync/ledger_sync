b367ca06b2fc7621dcad2601f8ab8d2aa5da3cb7
Ryan Jackson <ryanwjackson@gmail.com>
2020-05-30 18:15:51 +0200

    Remove comments

--- spec/ledgers/netsuite/customer/create_spec.rb
+++ spec/ledgers/netsuite/customer/create_spec.rb
@@ -6,25 +6,4 @@ support 'netsuite/shared_examples'
 
 RSpec.describe LedgerSync::Ledgers::NetSuite::Customer::Operations::Create do
   it_behaves_like 'a netsuite operation'
-  # include InputHelpers
-  # include NetSuiteHelpers
-
-  # let(:resource) do
-  #   LedgerSync::Ledgers::NetSuite::Customer.new(
-  #     customer_resource.except(:name).merge(
-  #       companyName: 'Company Name',
-  #       firstName: 'Test',
-  #       lastName: 'This',
-  #       subsidiary: LedgerSync::Ledgers::NetSuite::Subsidiary.new
-  #     )
-  #   )
-  # end
-  # let(:client) { netsuite_client }
-
-  # it_behaves_like 'an operation'
-  # it_behaves_like 'a successful operation',
-  #                 stubs: %i[
-  #                   stub_customer_find
-  #                   stub_customer_create
-  #                 ]
 end
