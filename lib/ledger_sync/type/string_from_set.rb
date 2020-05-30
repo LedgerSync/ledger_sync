9403d8a8393bf62f1f995cda9fff32e17c568212
Jozef Vaclavik <jozef@trifle.io>
2020-05-30 16:00:12 +0200

    Fix StringFromSet

--- lib/ledger_sync/type/string_from_set.rb
+++ lib/ledger_sync/type/string_from_set.rb
@@ -22,6 +22,12 @@ module LedgerSync
         )
       end
 
+      def cast_value(args = {})
+        value = args.fetch(:value)
+        return if value.blank?
+        value.strip
+      end
+
       def valid?(args = {})
         super && valid_value?(args)
       end
@@ -34,7 +40,7 @@ module LedgerSync
 
       def valid_value?(args = {})
         value = args.fetch(:value).try(:to_s)
-        return if value.nil?
+        return true if value.blank?
 
         @values.include?(value)
       end
