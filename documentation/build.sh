871a95c96837673d15d796abbcbc26d2ea3e0c43
Ryan Jackson <ryanwjackson@gmail.com>
2020-06-09 13:20:43 +0200

    build

--- documentation/build.sh
+++ documentation/build.sh
@@ -4,5 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 ruby generate.rb
 cd site
-bundle install
-bundle exec jekyll build -d ../../docs
+bundle exec jekyll build -d ../../.
