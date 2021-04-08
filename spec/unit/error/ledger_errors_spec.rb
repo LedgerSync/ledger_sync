753c8145d1a605d6ac703f01444624e2d2d8bfad
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2021-04-08 17:00:23 +0530

    teat(error): unit tests for ledger errors

--- spec/unit/error/ledger_errors_spec.rb
+++ spec/unit/error/ledger_errors_spec.rb
@@ -1,76 +1,71 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require_relative '../../support/unit_testing_shared_examples'
 
 module LedgerSync
   class Error
-
-    RSpec.shared_examples 'error_message_initializer' do |**args|
-      it 'should initialize when client and message is provided' do
-        expect(described_class.new(client: test_ledger_client, message: 'test error', **args)).to be_a_kind_of(Error)
-      end
-    end
-
-    RSpec.shared_examples 'basic_ledger_error' do |**args|
-      describe '#initialize' do
-        it_should_behave_like 'error_message_initializer', **args
-      end
-
-      it 'should not initialize when client is not provided' do
-        expect { described_class.new(message: 'test error', **args) }.to raise_error(ArgumentError)
-      end
-
-      it 'should not initialize when message is not provided' do
-        expect { described_class.new(client: test_ledger_client, **args) }.to raise_error(ArgumentError)
+    RSpec.shared_examples 'a base error' do
+      include_examples 'a required argument initializer', message: 'test', client: nil do
+        let(:let_args) { { client: test_ledger_client } }
       end
     end
 
     RSpec.describe LedgerError do
-      include_examples 'basic_ledger_error'
+      it_should_behave_like 'a base error'
     end
 
     class LedgerError
       [AuthenticationError, AuthorizationError, ConfigurationError].each do |sub_class|
         RSpec.describe sub_class do
-          include_examples 'basic_ledger_error'
-        end
-      end
-
-      RSpec.describe LedgerValidationError do
-        include_examples 'basic_ledger_error', attribute: 'test attribute', validation: 'test validation'
-
-        it 'should not initialize when attribute is not provided' do
-          expect { described_class.new(client: test_ledger_client, message: 'test_message', validation: 'test_validation') }.to raise_error(ArgumentError)
+          it_should_behave_like 'a base error'
         end
 
-        it 'should not initialize when validation is not provided' do
-          expect { described_class.new(client: test_ledger_client, message: 'test_message', attribute: 'test_attribute') }.to raise_error(ArgumentError)
-        end
-      end
-
-      RSpec.describe MissingLedgerError do
-        it 'should not initialize when client is not provided' do
-          # expect { described_class.new(message: 'test error') }.to raise_error(ArgumentError)
-        end
-      end
-
-      RSpec.describe ThrottleError do
-        it 'should initialize when client is provided' do
-          expect(described_class.new(client: test_ledger_client)).to be_a_kind_of(Error)
+        RSpec.describe MissingLedgerError do
+          include_examples 'a required argument initializer', message: 'test'
         end
 
-        it 'should have a default message' do
-          expect(described_class.new(client: test_ledger_client).message).to be_truthy
+        RSpec.describe LedgerValidationError do
+          it_should_behave_like 'a base error' do
+            let(:let_args) { { client: test_ledger_client, attribute: 'test attribute', validation: 'test validation' } }
+          end
         end
 
-        it 'should change default message when a custom message is provided' do
-          test_message = 'test message'
-          expect(described_class.new(client: test_ledger_client, message: test_message).message).to eq(test_message)
+        RSpec.describe ThrottleError do
+          include_examples 'a required argument initializer' do
+            let(:let_args) { { client: test_ledger_client } }
+          end
+
+          describe '.message' do
+            it 'should have a default message' do
+              expect(described_class.new(client: test_ledger_client).message).to be_truthy
+            end
+
+            it 'should change default message when a custom message is provided' do
+              test_message = 'test message'
+              expect(described_class.new(client: test_ledger_client, message: test_message).message).to eq(test_message)
+            end
+          end
+
+          describe '.response' do
+            it 'should respond to "response" when provided' do
+              test_response = 'test response'
+              expect(described_class.new(client: test_ledger_client, response: test_response).response).to eq(test_response)
+            end
+          end
         end
 
-        it 'should respond to "response" when provided' do
-          test_response = 'test response'
-          expect(described_class.new(client: test_ledger_client, response: test_response).response).to eq(test_response)
+        RSpec.describe UnknownURLFormat do
+
+          include_examples 'a required argument initializer' do
+            let(:test_resource) do
+              new_resource_class(
+                attributes: %i[test_key]
+              ).new(test_key: 'test_value',)
+            end
+
+            let(:let_args) { { client: test_ledger_client, resource: test_resource  } }
+          end
         end
       end
     end
