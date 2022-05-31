1059bd8dd53e4ede03834ca503b93f934d772073
Ryan <ryanwjackson@users.noreply.github.com>
2022-05-31 15:07:47 -0400

    Update docs build script

--- documentation/build.sh
+++ documentation/build.sh
@@ -4,7 +4,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd ..
 bundle install
+gem install jekyll jekyll-menus jekyll-paginate jekyll-paginate-v2
 cd documentation
 ruby generate.rb
 cd site
-bundle exec jekyll build -d ../../docs
+jekyll build -d ../../docs
